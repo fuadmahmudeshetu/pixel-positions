a561de4f71bc07d2ea4311bf6fe51acaec15cdde
fuadmahmudeshetu <nsr.412.15@amu.edu.et>
2026-06-24 23:16:41 +0300

    ok

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,7 +1,7 @@
 @props(['employer' => null, 'width' => 90])
 
 @php
-    $src = Vite::asset('resources/images/default-profile.svg');
+    $src = asset('images/default-profile.svg');
 @endphp
 
 <img src="{{ $src }}" alt="{{ $employer?->name ?? '' }}" {{ $attributes->merge(['class' => 'rounded-xl']) }} width="{{ $width }}" height="{{ $width }}">
