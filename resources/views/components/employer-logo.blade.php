f91a830f822e09cdf64a4b8e866e7d0c36673279
fuadmahmudeshetu <nsr.412.15@amu.edu.et>
2026-06-24 22:20:28 +0300

    ...

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -3,7 +3,7 @@
 @php
     $src = $employer && $employer->logo
         ? (str_starts_with($employer->logo, 'http') ? $employer->logo : asset('storage/' . $employer->logo))
-        : "https://picsum.photos/seed/" . rand(0, 10000) . "/" . $width;
+        : "https://via.placeholder.com/" . $width . "?text=" . urlencode(substr($employer?->name ?? 'Logo', 0, 2));
 @endphp
 
 <img src="{{ $src }}" alt="{{ $employer?->name ?? '' }}" {{ $attributes->merge(['class' => 'rounded-xl']) }} width="{{ $width }}">
