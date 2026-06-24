657b04e83909b25ff96f6a3e7b7b04999fff4cc3
fuadmahmudeshetu <nsr.412.15@amu.edu.et>
2026-06-24 23:11:07 +0300

    profile update

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,9 +1,7 @@
 @props(['employer' => null, 'width' => 90])
 
 @php
-    $src = $employer && $employer->logo
-        ? (str_starts_with($employer->logo, 'http') ? $employer->logo : asset('storage/' . $employer->logo))
-        : "https://via.placeholder.com/" . $width . "?text=" . urlencode(substr($employer?->name ?? 'Logo', 0, 2));
+    $src = Vite::asset('resources/images/default-profile.svg');
 @endphp
 
-<img src="{{ $src }}" alt="{{ $employer?->name ?? '' }}" {{ $attributes->merge(['class' => 'rounded-xl']) }} width="{{ $width }}">
+<img src="{{ $src }}" alt="{{ $employer?->name ?? '' }}" {{ $attributes->merge(['class' => 'rounded-xl']) }} width="{{ $width }}" height="{{ $width }}">
